8edea7592c6acf9ccff752a6633617a79303ccc1
Miek Gieben <miek@miek.nl>
2016-08-18 21:35:34 -0700

    coredns.go smaller

--- coredns.go
+++ coredns.go
@@ -1,13 +1,8 @@
 package main
 
+import "github.com/mholt/caddy/caddy/caddymain"
+
 //go:generate go run plugin_generate.go
-
-import (
-	"github.com/mholt/caddy/caddy/caddymain"
-)
-
-var run = caddymain.Run
-
 func main() {
-	run()
+	caddymain.Run()
 }
